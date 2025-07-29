939c04e95bf761863c1fc313b081ce1805e22fc7
sysong0516 <sysong777@naver.com>
2025-07-29 18:00:57 +0900

    game, ranking, diff css

--- src/pages/Difficulty.jsx
+++ src/pages/Difficulty.jsx
@@ -7,42 +7,45 @@ import { changeDiff } from '../redux/diff';
 import { Button } from 'react-bootstrap';
 import './Difficulty.css'
 
-function Difficulty(){
-  const diff = useSelector(state=>state.diff)
+function Difficulty() {
+  const diff = useSelector(state => state.diff)
   const dispatch = useDispatch()
   const [radioValue, setRadioValue] = useState(diff);
-  
+
 
   const radios = [
     { name: '쉬움', value: '0' },
     { name: '어려움', value: '1' },
   ];
 
-  return(
-    <>
-      <p>난이도를 설정해주세요</p>
-      <ButtonGroup className="mb-2">
-        {radios.map((radio, idx) => (
-          <ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant="secondary"
-            name="radio"
-            value={radio.value}
-            checked={radioValue === radio.value}
-            onChange={(e) => setRadioValue(e.currentTarget.value)}
-          >
-            {radio.name}
-          </ToggleButton>
-        ))}
-      </ButtonGroup>
-      <br/>
-      <Button onClick={()=>{
-        dispatch(changeDiff(radioValue))
-        alert('적용되었습니다')
-      }}>적용</Button>
-    </>
+  return (
+    <div className='diff-bg'>
+      <div className='diff-container'>
+        <h1>난이도를 설정해주세요</h1>
+        <ButtonGroup className="mb-2">
+          {radios.map((radio, idx) => (
+            <ToggleButton
+              className='diff-toggle'
+              key={idx}
+              id={`radio-${idx}`}
+              type="radio"
+              variant="secondary"
+              name="radio"
+              value={radio.value}
+              checked={radioValue === radio.value}
+              onChange={(e) => setRadioValue(e.currentTarget.value)}
+            >
+              {radio.name}
+            </ToggleButton>
+          ))}
+        </ButtonGroup>
+        <br />
+        <Button className='diff-button' onClick={() => {
+          dispatch(changeDiff(radioValue))
+          alert('적용되었습니다')
+        }}>적용</Button>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -13,7 +13,6 @@ function shuffle() {
   numbers = Array.from({ length: 1000 }, (_, i) => i);
   shuffled = numbers.sort(() => Math.random() - 0.5);
   randomSelection = shuffled.slice(350, 355);
-  console.log('난수생성 ' + randomSelection)
   randomSelection.map((num, i) => {
     quizArray[i + 1] = quiz[num]
   })
@@ -45,17 +44,17 @@ function Game({ name, score, setScore }) {
   useEffect(() => {
     if (index == 6) {
       let Rank = localStorage.getItem('rank');
-      let userResult=[{rank: 1, name:name, score:score, time:new Date().toLocaleTimeString()}];
-      if(Rank!=null){
-        Rank=JSON.parse(Rank);
+      let userResult = [{ rank: 1, name: name, score: score, time: new Date().toLocaleTimeString() }];
+      if (Rank != null) {
+        Rank = JSON.parse(Rank);
         Rank.push(userResult[0])
-        Rank.sort((a,b)=>b.score-a.score)
-        Rank.map((data,i)=>{
-          data.rank=i+1;
+        Rank.sort((a, b) => b.score - a.score)
+        Rank.map((data, i) => {
+          data.rank = i + 1;
         })
         localStorage.setItem('rank', JSON.stringify(Rank));
       }
-      else{
+      else {
         localStorage.setItem('rank', JSON.stringify(userResult));
       }
       shuffle();
@@ -70,10 +69,10 @@ function Game({ name, score, setScore }) {
     }, 1000);
 
     // Clean-up 함수 (컴포넌트 언마운트 시 타이머 해제 및 점수초기화)
-    return () =>{
+    return () => {
       setScore(0);
       clearInterval(timer);
-    } 
+    }
   }, []);
 
   useEffect(() => {
@@ -86,36 +85,44 @@ function Game({ name, score, setScore }) {
   if (index == 0) {
     return (
       <>
-        <h3>잠시후 퀴즈가 시작됩니다</h3>
+        <div className="game-bg">
+          <div className="game-container">
+            <h3>잠시후 퀴즈가 시작됩니다</h3>
+            <h4>{timeLeft}</h4>
+          </div>
+        </div>
       </>
     )
   }
 
   if (index == 6) {
     return (
-      <>
-        <h4>종료되었습니다.</h4>
-      </>
+      <div className="game-bg">
+        <div className="game-container">
+          <h4>종료되었습니다.</h4>
+        </div>
+      </div>
     )
   }
 
 
   return (
-    <>
-      <div>
-        <h3>{quizArray[index].question}</h3>
-        <button onClick={() => { chk(0) }}>{quizArray[index].choices[0].korean}</button>
-        <button onClick={() => { chk(1) }}>{quizArray[index].choices[1].korean}</button>
-        <button onClick={() => { chk(2) }}>{quizArray[index].choices[2].korean}</button>
-        <button onClick={() => { chk(3) }}>{quizArray[index].choices[3].korean}</button>
+    <div className="game-bg">
+      <div className="game-container">
+        <div>
+          <h4 className="quiz">{quizArray[index].question}</h4>
+          <button onClick={() => { chk(0) }}>{quizArray[index].choices[0].korean}</button>
+          <button onClick={() => { chk(1) }}>{quizArray[index].choices[1].korean}</button>
+          <button onClick={() => { chk(2) }}>{quizArray[index].choices[2].korean}</button>
+          <button onClick={() => { chk(3) }}>{quizArray[index].choices[3].korean}</button>
+        </div>
+        <br />
+        <h4>남은 시간: {timeLeft}초</h4>
+        <p>Score : {score}</p>
+        <p>남은 문제: {6 - index}</p>
+        <p>Level : {diff.count == 1 ? '어려움' : '쉬움'}</p>
       </div>
-      <br />
-      <h4>남은 시간: {timeLeft}초</h4>
-      <p>현재 점수는 {score}</p>
-      <p>{name}님의 남은 문제는 {6 - index}</p>
-      <p>현재 난이도는 {diff.count == 1 ? '어려움' : '쉬움'}</p>
-    </>
-
+    </div>
   )
 }
 
