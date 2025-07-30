1395238aaebcd30961de647920d489eef9ad7304
sysong0516 <sysong777@naver.com>
2025-07-30 17:57:00 +0900

    config설정변경

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link, useNavigate } from "react-router-dom";
+import './Header.css'
+
+function Header() {
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" className='head'>
+        <Container>
+          <Navbar.Brand onClick={() => navigate('/')}>너는맞추어라영단어를</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => navigate('/rank')}>랭킹</Nav.Link>
+            <Nav.Link onClick={() => navigate('/difficulty')}>난이도설정</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Header
